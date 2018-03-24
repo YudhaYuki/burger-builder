2b3c8fb810eb0d7c4fd01490a9ee40efa91644ad
YudhaYuki <Yudha.Yuki@hotmail.com>
2018-03-24 20:11:17 +0100

    Creating a Layout Component - STEP I

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Layout = () => (
+    <div> Toolbar, SideDrawer, Backdrop </div>
+    <main>
+        {props.children}
+    </main>
+);
+
+export default Layout;
