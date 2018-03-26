c219828156935d27fed8a65e1ba156c3dbbda5f3
YudhaYuki <Yudha.Yuki@hotmail.com>
2018-03-26 12:02:23 +0200

    Outputting Burget Ingredients Dynamically - Transform an object of key value pairs into an array of BurgerIngredient

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,15 +3,23 @@ import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const burger = (props) => {
+const burger = (props) => { 
+    // Transform an object of key value pairs into an array of BurgerIngredient where the value of that object is important for me to decide how many ingredients I need and the key is important for which type of ingredient I need.
+    const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredient key={igKey + 1} type={igKey} />
+        });
+    });
+
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
 };
 
 export default burger;
+
