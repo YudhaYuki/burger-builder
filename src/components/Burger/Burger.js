2cad0e3c5056cc7413ece949bda448566b2c4511
YudhaYuki <Yudha.Yuki@hotmail.com>
2018-03-26 14:12:02 +0200

    Calculating the ingredient Sum Dynamically - Reduce allows us to transform an array into something else

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,12 +5,21 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => { 
     // Transform an object of key value pairs into an array of BurgerIngredient where the value of that object is important for me to decide how many ingredients I need and the key is important for which type of ingredient I need.
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_, i) => {
-            return <BurgerIngredient key={igKey + 1} type={igKey} />
+            return <BurgerIngredient key={igKey + i} type={igKey} />;
         });
-    });
+    })
+
+    // Reduce allows us to transform an array into something else
+    .reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p> Please start adding ingredients! </p>
+    }
 
     return(
         <div className={classes.Burger}>
