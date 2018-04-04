af73c23a16a29844347f4304268afb20c2aeb32e
YudhaYuki <YudhaYuki@hotmail.com>
2018-04-04 12:55:48 +0200

    Adding a Toolbar

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -6,7 +6,7 @@ import Button from '../../UI/Button/Button';
 const orderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients)
         .map(igKey => {
-            return (<li>
+            return (<li key={igKey}>
                         <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
                     </li>);
         });
@@ -18,7 +18,7 @@ const orderSummary = (props) => {
                 {ingredientSummary}
             </ul>
             <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to checkout?</p>
+            <p>Continue to Checkout?</p>
             <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
             <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
         </Aux>
