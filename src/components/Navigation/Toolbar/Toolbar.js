3f40f48f42df8267dd8c7588095151dbbf0ea715
YudhaYuki <YudhaYuki@hotmail.com>
2018-04-04 14:43:48 +0200

    Working on Responsive Adjustments

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -8,7 +8,11 @@ const toolbar = (props) => (
 
     <header className = {classes.Toolbar}>
         <div>MENU</div>
-        <Logo />
+
+        <div className={classes.Logo}>
+            <Logo/>
+        </div>
+
         <nav>
             <NavigationItems />
         </nav>
